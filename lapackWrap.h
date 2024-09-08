3351603fb8759b649a4abc224e300f393bc17676
Beomjoon Goh <goh.beomjoon@gmail.com>
2024-09-09 01:29:53 +0900

    Add dggev routine for generalized eigenvalue problem

--- lapackWrap.h
+++ lapackWrap.h
@@ -11,6 +11,7 @@ namespace LAPACK {
     void zgetri_(int *N, compdb *A, int *LDA, int *IPIV, compdb *WORK, int *LWORK, int *INFO);
     void dsyev_ (char *JOBZ, char *UPLO, int *N, double *A, int *LDA, double *W, double *WORK, int *LWORK, int *info);
     void zheev_ (char *JOBZ, char *UPLO, int *N, compdb *A, int *LDA, double *W, compdb *WORK, int *LWORK, double *RWORK, int *info);
+    void dggev_ (char *JOBVL, char *JOBVR, int *N, double *A, int *LDA, double *B, int *LDB, double *ALPHAR, double *ALPHAI, double *BETA, double *VL, int *LDVL, double *VR, int *LDVR, double *WORK, int *LWORK, int *INFO);
   }
   inline void xgetrf_(int *M, int *N, double *A, int *LDA, int *IPIV, int *INFO) { dgetrf_(M, N, A, LDA, IPIV, INFO); }
   inline void xgetrf_(int *M, int *N, compdb *A, int *LDA, int *IPIV, int *INFO) { zgetrf_(M, N, A, LDA, IPIV, INFO); }
